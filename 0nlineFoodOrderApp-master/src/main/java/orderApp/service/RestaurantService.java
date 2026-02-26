b0c9c135cc888cceb8b5ff23c787a0840d02990f
Aishwarya patil <aishwaryapatil0021@gmail.com>
2026-02-26 18:32:04 +0530

    Initial commit

---
+++ 0nlineFoodOrderApp-master/src/main/java/orderApp/service/RestaurantService.java
@@ -0,0 +1,22 @@
+package orderApp.service;
+
+import java.util.Set;
+
+import org.springframework.data.domain.Page;
+
+import orderApp.entity.Restaurant;
+
+public interface RestaurantService {
+	
+	Restaurant createRestaurant(Restaurant restaurant);
+	
+	Restaurant getById(Integer id);
+	
+	Page getAllRestaurants(int pageNum,int pageSize, String sortBy);
+	
+	Restaurant updateRestaurant(Integer id,Restaurant restaurant);
+	
+	void deleteRestaurant(Integer id);
+	
+	Restaurant assignFood(Integer restaurantId,Set<Integer> foodId);
+}
